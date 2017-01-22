6b21c373de7208798cc4859aca06aa92af5adc69
liushuo <manunited_22@sina.com>
2017-01-22 17:10:36 +0800

    修改生成图片名称出现问题 对文件名称进行过滤

--- middle/savePoster.js
+++ middle/savePoster.js
@@ -4,9 +4,9 @@ var fs = require('fs'),
 
 var savePoster = function (movieDir, movieNames, urls) {
     for(var movieNum in movieNames){
-        request.get(urls[movieNum]).pipe(fs.createWriteStream(path.join(movieDir, movieNames[movieNum] + '.jpg')))
+        let thisName = path.parse(movieNames[movieNum]).name;
+        request.get(urls[movieNum]).pipe(fs.createWriteStream(path.join(movieDir, thisName + '.jpg')))
     }
-    // request.get(url).pipe(fs.createWriteStream(path.join(movieDir, movieName + '.jpg')))
 }
 
 module.exports = savePoster;
